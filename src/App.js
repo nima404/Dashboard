ce7051a4d7ad8fbb77ef76805d9eca55d13898f9
nima tajik <nima.error404@gmail.com>
2022-10-15 13:09:29 +0330

    feat: ADD REACT PROJECT

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+
+    <div className="App">
+      alireza mohammadkhani
+      <p>mahdie</p>
+    </div>
+
+
+  );
+}
+
+export default App;
